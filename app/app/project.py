a076106ef4e8a44c8ed810a73c0596950216e669
Ivan Ramirez <ivan.ramirez@capek.ai>
2019-07-18 22:22:11 -0500

    Add new functions

--- app/app/project.py
+++ app/app/project.py
@@ -82,3 +82,52 @@ def loadFile():
             document.update({header: str(data[position]), 'type': 'product'})
 
         conect.upsert(ut.generate_id(), document)
+
+
+# Media
+def average(data_list):
+    return round(float(sum(data_list) / len(data_list)), 2)
+
+
+# Mediana
+def mediana(data_list):
+    data_list.sort()
+
+    if len(data_list) % 2 == 0:
+        n = len(data_list)
+        return (data_list[n / 2 - 1] + data_list[n / 2]) / 2
+    else:
+        return data_list[len(data_list) / 2]
+
+
+# DesStd
+def DesStd(data_list):
+    n = len(data_list)
+
+    promedio = average(data_list)
+    cuadrados = []
+    for dato in data_list:
+        r = (dato - promedio) ** 2
+        cuadrados.append(r)
+
+    return (sum(cuadrados) / (n - 1)) ** 0.5
+
+
+# Varianza
+def varianza(data_list):
+    suma = 0
+    m = average(data_list)
+    for elemento in data_list:
+        suma += (elemento - m) ** 2
+
+    return suma / float(len(data_list))
+
+
+# Max
+def max_data(data_list):
+    return max(data_list)
+
+
+# Min
+def min_data(data_list):
+    return min(data_list)
